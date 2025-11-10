dc6a476943fd40fbfc8f3ccd09461d1d7583b4c6
Lê Sỹ Tuyền <130513663+hatohui@users.noreply.github.com>
2025-11-10 07:50:24 +0700

    feat: add new components for admin and talent sections, including layout and pages

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type AdminLayoutProps = {
+	revenue: React.JSX.Element
+	timeline: React.JSX.Element
+}
+
+const AdminLayout = ({ revenue, timeline }: AdminLayoutProps) => {
+	return (
+		<>
+			<section>{revenue}</section>
+			<section>{timeline}</section>
+			<section>Admin Page</section>
+		</>
+	)
+}
+
+export default AdminLayout
---
+++ src/app/talent/@form/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const TalentForm = () => {
+	return <div>TalentForm</div>
+}
+
+export default TalentForm
