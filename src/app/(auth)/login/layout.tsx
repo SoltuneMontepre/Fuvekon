2e8d337650eedf517bb89e81fb4c7f4bb7c6d8f6
KietPham-VN <phamanhkiet.dev@gmail.com>
2025-11-12 10:08:47 +0700

    feat(LoginLayout): enhance layout structure and integrate background for improved visual appeal

--- src/app/(auth)/login/layout.tsx
+++ src/app/(auth)/login/layout.tsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import Background from '@/components/landing/hero_section/Background'
 
 type LoginLayoutProps = {
 	form: React.ReactElement
 }
 
 const LoginLayout = ({ form }: LoginLayoutProps): React.ReactElement => {
-	return <>{form}</>
+	return (
+		<div className='relative min-h-screen w-full flex items-center justify-center overflow-hidden px-4 sm:px-6 lg:px-8'>
+			<div className='fixed inset-0 w-full h-full z-[-10]'>
+				<div className='absolute inset-0 blur-sm scale-110'>
+					<Background />
+				</div>
+			</div>
+			{form}
+		</div>
+	)
 }
 
 export default LoginLayout
