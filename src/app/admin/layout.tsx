68d4bb3ed8c6240f495cab2c72bc2d9c9b6a009c
Lê Sỹ Tuyền <130513663+hatohui@users.noreply.github.com>
2025-11-13 18:20:54 +0700

    feat: add children prop to AdminLayout for improved layout flexibility

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -3,13 +3,15 @@ import React from 'react'
 type AdminLayoutProps = {
 	revenue: React.JSX.Element
 	timeline: React.JSX.Element
+	children: React.JSX.Element
 }
 
-const AdminLayout = ({ revenue, timeline }: AdminLayoutProps) => {
+const AdminLayout = ({ revenue, timeline, children }: AdminLayoutProps) => {
 	return (
 		<>
 			<section>{revenue}</section>
 			<section>{timeline}</section>
+			<section>{children}</section>
 		</>
 	)
 }
