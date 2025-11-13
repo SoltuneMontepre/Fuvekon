3bbabbc8f11d140ebb16c5efeb131b04ace94c46
Lê Sỹ Tuyền <130513663+hatohui@users.noreply.github.com>
2025-11-13 18:20:01 +0700

    feat: refactor ScrollBar component to use button for better accessibility

--- src/components/common/scroll/ScrollBar.tsx
+++ src/components/common/scroll/ScrollBar.tsx
@@ -3,14 +3,19 @@ import React from 'react'
 
 const ScrollBar = ({ onClick }: { onClick: () => void }) => {
 	return (
-		<Image
-			className='scroll-bar'
-			src='/components/scroll-bar.png'
-			alt='scroll bar'
-			width={1728}
-			height={64}
+		<button
+			className='scroll-bar border-0 p-0 bg-transparent cursor-pointer'
 			onClick={onClick}
-		/>
+			aria-label='Toggle scroll content'
+		>
+			<Image
+				src='/components/scroll-bar.png'
+				alt=''
+				width={1728}
+				height={64}
+				className='max-w-full h-auto'
+			/>
+		</button>
 	)
 }
 
